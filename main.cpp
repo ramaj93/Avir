6c4b1a82f21952cb489a518ee35eb7dff28af6ac
ramaj93 <ramaj93@yahoo.com>
2014-05-22 23:45:59 +0300

    More file search capabilities

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include "resource.h"
 #include <string.h>
 #include <vector>
+#include <string>
+#include <cstdio>
 
 HINSTANCE hInst;
 HANDLE mhThread;
@@ -12,9 +14,26 @@ bool pause = false;
 char drivers[MAX_PATH];
 char drive[25][10];
 long det = 0;
+FILE *logFile = NULL;
+const unsigned int nDefs = IDS_LAST - IDS_DECT;
 CRITICAL_SECTION cs;
 
 typedef std::vector<HANDLE> vHandle;
+typedef std::vector<std::string> vString;
+std::vector<int> vLevels;
+
+struct def
+{
+    vString files;
+    vString regs;
+};
+
+typedef std::vector<def> vDef;
+
+vDef defs;
+vString roots;
+
+void prepString();
 int fillDrives(int len)
 {
     int drv = 0;
@@ -32,69 +51,117 @@ int fillDrives(int len)
     return drv;
 }
 
+const unsigned int MAX_LEV = 2;
+
 DWORD WINAPI thread(LPVOID param)
 {
     HWND lHwnd = (HWND)param;
-    long long count = 0;
+    int count = 0;
     char buf[MAX_PATH];
     int len = GetLogicalDriveStrings(MAX_PATH,drivers);
     WIN32_FIND_DATA fd;
     HANDLE file = NULL;
     vHandle handles;
 
+    /*We always start at the drive root*/
     int level = 0;
     /*Get Number of Drives and Root Paths*/
 
     int dlen = fillDrives(len);
     int rt = 1;
+    std::string curFolder;
+
+    logFile = fopen("log.txt","a");
 
     while(!Stop &&count <=dlen)
     {
-        /*Set Current directory
-        if folder open*/
+        /*Set Current directory*/
         SetCurrentDirectory(drive[count]);
+        //roots.push_back(drive[count]); //pushed the root
         if(file == NULL)file = FindFirstFile("*",&fd);
-        while(rt&&file&&!Stop)
+        //if file erro deal
+        char e[MAX_PATH] ;
+
+        while(!Stop)          //we shouldnt get out of this loop until we finish the drive
         {
             if(!pause)
             {
                 Sleep(10);
                 rt = FindNextFile(file,&fd);
-                if(rt > 0)
+
+                GetCurrentDirectory(MAX_PATH,e);
+                char *hy = fd.cFileName;
+                if(rt > 0) //not the end of root
                 {
-                    if(level == 0 &&(fd.dwFileAttributes&FILE_ATTRIBUTE_DIRECTORY))
+                    if(fd.dwFileAttributes&FILE_ATTRIBUTE_DIRECTORY)
                     {
                         //Its a directory
-                        //we'll only go in one level
-                        handles.push_back(file);
-                        SetCurrentDirectory(fd.cFileName);
-                        file = FindFirstFile("*",&fd);
-                        level++;
+                        //we'll only go to max level
+                        if(level < MAX_LEV)
+                        {
+                            if(fd.cFileName[0] == '.')continue;
+                            handles.push_back(file);
+                            GetCurrentDirectory(MAX_PATH,e);
+                            roots.push_back(e);
+                            char *fld = fd.cFileName;
+                            SetCurrentDirectory(fd.cFileName);
+                            curFolder = fd.cFileName;       //save the current dir
+                            //roots.push_back(fd.cFileName);  //push the new root
+                            file = FindFirstFile("*",&fd);  //get the new handle
+                            vLevels.push_back(level);       //push the level
+                            level++;
+                        }
+                        else continue;   //goto lower level
                     }
                     else
                     {
                         //its a file
+                        if(fd.cFileName[0] == '.')continue; //jump the . & ..
                         char path[MAX_PATH];
                         GetFullPathName(fd.cFileName,MAX_PATH,path,NULL);
                         SendMessage(lHwnd,WM_UPDATE_MSG,0,(LPARAM)path);
-                        //do what to do here
-                        if(strcmp(fd.cFileName,"Autoit.exe") == 0)PostMessage(lHwnd,WM_UPDATE_DET,0,0);
+                        int id = defs.size();
+                        for(int i = 0; i<defs.size(); i++)
+                        {
+                            for(int j = 0; j<defs[i].files.size(); j++)
+                            {
+                                if(strcmp(fd.cFileName,defs[i].files[j].c_str()) == 0)
+                                {
+                                    PostMessage(lHwnd,WM_UPDATE_DET,0,0);
+                                    fprintf(logFile,"%s\n",fd.cFileName);
+                                }
+
+                            }
+                        }
+                        if(curFolder.compare(fd.cFileName)==0)
+                        {
+                            PostMessage(lHwnd,WM_UPDATE_DET,0,0);
+                            fprintf(logFile,"%s\n",fd.cFileName);
+                        }
                         PostMessage(lHwnd,WM_STEP_PB,0,0);
                     }
                     memset(&fd,0,sizeof(fd));
                 }
-                else
+                else //rt is zero ff has failed
                 {
                     //going back one level will return us to the root
-                    if(level > 0)
+                    if(level > 0) //if its not drive root
                     {
-                        SetCurrentDirectory(drive[count]);
+                        char *str = (char*)roots.back().c_str();
+                        //roots.pop_back();                       //pop this root to the previous one
+                        str = (char*)roots.back().c_str();
+                        GetCurrentDirectory(MAX_PATH,e);
+                        SetCurrentDirectory(str);
+                        GetCurrentDirectory(MAX_PATH,e);
+                        roots.pop_back();
                         file = handles.back();
                         handles.pop_back();
-                        level = 0;
-                        rt = FindNextFile(file,&fd);
+                        level--;
+                        vLevels.pop_back();
+                        //rt = FindNextFile(file,&fd); //get the ff of the prev handle
                     }
-                    else break;
+                    else break; //exit loop weve hit the end of drive
+                    memset(&fd,0,sizeof(fd));
                 }
             }
             else
@@ -109,9 +176,45 @@ DWORD WINAPI thread(LPVOID param)
     }
     if(Stop)PostMessage(lHwnd,WM_UPDATE_MSG,0,(LPARAM)("Stopped"));
     else PostMessage(lHwnd,WM_UPDATE_MSG,0,(LPARAM)("Finished"));
+    SendMessage(lHwnd,WM_CHK_DONE,0,0);
+    fflush(logFile);
+    fclose(logFile);
     ExitThread(0);
 }
 
+void prepString()
+{
+    def tmp;
+    char *strBuf;
+    int flag = 0;
+    for(int i = 1; i<nDefs; i++)
+    {
+        char Buf[512] = {0};
+        LoadString(hInst,IDS_DECT+i,Buf,512);
+        strBuf = strtok(Buf,"~");
+        tmp.files.push_back(strBuf);
+        while((strBuf = strtok(NULL,"~"))!= NULL)
+        {
+            if(strBuf[0] == 'r')
+            {
+                //its a reg string
+                flag = 1;
+            }
+            else
+            {
+                if(flag == 1)
+                {
+                    tmp.regs.push_back(strBuf);
+                    flag = 0;
+                }
+                else tmp.files.push_back(strBuf);
+            }
+        }
+        defs.push_back(tmp);
+        tmp.files.clear();
+        tmp.regs.clear();
+    }
+}
 BOOL CALLBACK DlgMain(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch(uMsg)
@@ -120,7 +223,18 @@ BOOL CALLBACK DlgMain(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
     {
         HICON t = LoadIcon(GetModuleHandle(NULL),MAKEINTRESOURCE(IDI_MYICON));
         SendMessage(hwndDlg,WM_SETICON,ICON_BIG,(LPARAM)t);
+        char tmp[8];
+        itoa(nDefs,tmp,10);
+        SetWindowText(GetDlgItem(hwndDlg,IDS_NDEF),tmp);
         hInst = GetModuleHandle(NULL);
+        prepString();
+        free(tmp);
+    }
+    return TRUE;
+
+    case WM_CHK_DONE:
+    {
+        SendMessage(GetDlgItem(hwndDlg,IDI_PROG),PBM_SETPOS,(WPARAM)100,0);
     }
     return TRUE;
 
--- resource.h
+++ resource.h
@@ -3,16 +3,20 @@
 #define     WM_UPDATE_MSG                       WM_USER+0
 #define     WM_STEP_PB                          WM_USER+2
 #define     WM_UPDATE_DET                       WM_USER+3
+#define     WM_CHK_DONE                         WM_USER+4
 #endif
 
 #define DLG_MAIN                                100
-#define DF_SKYPEE                               40000
+
 #define IDSCAN                                  40000
 #define IDPAUSE                                 40001
-#define REG_SKYPEE                              40001
-#define FL_TEST                                 40002
 #define IDREPAIR                                40002
 #define IDMSG                                   40003
 #define IDI_PROG                                40004
 #define IDI_MYICON                              40005
-#define IDS_DECT                                40005
+#define IDS_NDEF                                40006
+
+#define IDS_DECT                                500
+#define DF_SKYPEE                               501
+#define FL_TEST                                 502
+#define IDS_LAST                                503
